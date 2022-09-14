cc728ce50817a7e4cad806fc47ba60dc52baaed3
edvin <edvin@DESKTOP-QHD6AI8>
2022-09-14 16:39:28 +0200

    Add project files.

---
+++ Uppgift 2.15/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en mening med endast tre ord och en punkt");
+            Console.Write("Skriv här: "); string mening = Console.ReadLine();
+
+            string[] ord = mening.Split(' ', '.');
+
+            Console.WriteLine("Din mening baklänges blir då: " + ord[2] + " " + ord[1] + " " + ord[0] + ".");
+        }
+    }
+}
